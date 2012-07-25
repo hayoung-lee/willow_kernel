4d0445134deda24eb99552f86b099c31ae4a9d0c
kimsh <kimsh@kimsh-P43-ES3G.(none)>
2012-07-25 18:29:58 +0900

    [chg] touch i2c-5 sda_delay default value로 설정

--- arch/arm/plat-samsung/dev-i2c5.c
+++ arch/arm/plat-samsung/dev-i2c5.c
@@ -54,14 +54,14 @@ static struct s3c2410_platform_i2c touch_100k_i2c_data __initdata = {
 	.flags		= 0,
 	.slave_addr	= 0x10,
 	.frequency	= 100*1000,
-	.sda_delay	= S3C2410_IICLC_SDA_DELAY5 | S3C2410_IICLC_FILTER_ON,
+	.sda_delay	= 100,
 };
 
 static struct s3c2410_platform_i2c touch_300k_i2c_data __initdata = {
 	.flags		= 0,
 	.slave_addr	= 0x10,
 	.frequency	= 300*1000,
-	.sda_delay	= S3C2410_IICLC_SDA_DELAY5 | S3C2410_IICLC_FILTER_ON,
+	.sda_delay	= 100,
 };
 
 void __init s3c_i2c5_set_platdata(struct s3c2410_platform_i2c *pd)
