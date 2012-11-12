d93d0a409c3e80d65d05c644ce1db23a8a1b8dc6
koohyunYu <koohyun.yu@gmail.com>
2012-11-12 11:23:27 +0900

    [chg] update mkbootimg for sw1 [willow_jb_android_sw1]

--- mkbootimg.sh
+++ mkbootimg.sh
@@ -12,3 +12,10 @@ cp arch/arm/boot/zImage ../willow_jb_android/device/thinkware/willow/
 cp drivers/net/wireless/bcmdhd/bcmdhd.ko ../willow_jb_android/out/target/product/willow/system/lib/modules/
 cp drivers/net/wireless/bcmdhd/bcmdhd.ko ../willow_jb_android/device/thinkware/willow/
 
+# JB SW1
+cp arch/arm/boot/zImage ../willow_jb_android_sw1/device/thinkware/willow/
+../willow_jb_android_sw1/out/host/linux-x86/bin/mkbootimg --kernel arch/arm/boot/zImage --ramdisk ../willow_jb_android_sw1/out/target/product/willow/ramdisk.img --cmdline "console=ttySAC2,115200" --base 0x40008000 --pagesize 4096 --output ../willow_jb_android_sw1/out/target/product/willow/boot.img
+../willow_jb_android_sw1/out/host/linux-x86/bin/mkbootimg --kernel arch/arm/boot/zImage --ramdisk ../willow_jb_android_sw1/out/target/product/willow/ramdisk-recovery.img --cmdline "console=ttySAC2,115200" --base 0x40008000 --pagesize 4096 --output ../willow_jb_android_sw1/out/target/product/willow/recovery.img
+cp drivers/net/wireless/bcmdhd/bcmdhd.ko ../willow_jb_android_sw1/out/target/product/willow/system/lib/modules/
+cp drivers/net/wireless/bcmdhd/bcmdhd.ko ../willow_jb_android_sw1/device/thinkware/willow/
+
