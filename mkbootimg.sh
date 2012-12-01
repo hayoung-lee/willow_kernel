0209a780f127637f161142bcebf9b258f89d1417
shinnck <shinnck@ubivelox.com>
2012-12-01 20:27:23 +0900

    [chg] update bcmdhd

--- mkbootimg.sh
+++ mkbootimg.sh
@@ -2,20 +2,14 @@ cp arch/arm/boot/zImage ../img/
 cp arch/arm/boot/zImage ../willow_android/device/thinkware/willow/
 ../willow_android/out/host/linux-x86/bin/mkbootimg --kernel arch/arm/boot/zImage --ramdisk ../willow_android/out/target/product/willow/ramdisk.img --cmdline "console=ttySAC2,115200" --base 40008000 --pagesize 4096 -o ../willow_android/out/target/product/willow/boot.img
 ../willow_android/out/host/linux-x86/bin/mkbootimg --kernel arch/arm/boot/zImage --ramdisk ../willow_android/out/target/product/willow/ramdisk-recovery.img --cmdline "console=ttySAC2,115200" --base 40008000 --pagesize 4096 -o ../willow_android/out/target/product/willow/recovery.img
-cp drivers/net/wireless/bcmdhd/bcmdhd.ko ../willow_android/out/target/product/willow/system/lib/modules/
-cp drivers/net/wireless/bcmdhd/bcmdhd.ko ../willow_android/device/thinkware/willow/
 
 # JB
 cp arch/arm/boot/zImage ../willow_jb_android/device/thinkware/willow/
 ../willow_jb_android/out/host/linux-x86/bin/mkbootimg --kernel arch/arm/boot/zImage --ramdisk ../willow_jb_android/out/target/product/willow/ramdisk.img --cmdline "console=ttySAC2,115200" --base 0x40008000 --pagesize 4096 --output ../willow_jb_android/out/target/product/willow/boot.img
 ../willow_jb_android/out/host/linux-x86/bin/mkbootimg --kernel arch/arm/boot/zImage --ramdisk ../willow_jb_android/out/target/product/willow/ramdisk-recovery.img --cmdline "console=ttySAC2,115200" --base 0x40008000 --pagesize 4096 --output ../willow_jb_android/out/target/product/willow/recovery.img
-cp drivers/net/wireless/bcmdhd/bcmdhd.ko ../willow_jb_android/out/target/product/willow/system/lib/modules/
-cp drivers/net/wireless/bcmdhd/bcmdhd.ko ../willow_jb_android/device/thinkware/willow/
 
 # JB SW1
 cp arch/arm/boot/zImage ../willow_jb_android_sw1/device/thinkware/willow/
 ../willow_jb_android_sw1/out/host/linux-x86/bin/mkbootimg --kernel arch/arm/boot/zImage --ramdisk ../willow_jb_android_sw1/out/target/product/willow/ramdisk.img --cmdline "console=ttySAC2,115200" --base 0x40008000 --pagesize 4096 --output ../willow_jb_android_sw1/out/target/product/willow/boot.img
 ../willow_jb_android_sw1/out/host/linux-x86/bin/mkbootimg --kernel arch/arm/boot/zImage --ramdisk ../willow_jb_android_sw1/out/target/product/willow/ramdisk-recovery.img --cmdline "console=ttySAC2,115200" --base 0x40008000 --pagesize 4096 --output ../willow_jb_android_sw1/out/target/product/willow/recovery.img
-cp drivers/net/wireless/bcmdhd/bcmdhd.ko ../willow_jb_android_sw1/out/target/product/willow/system/lib/modules/
-cp drivers/net/wireless/bcmdhd/bcmdhd.ko ../willow_jb_android_sw1/device/thinkware/willow/
 
